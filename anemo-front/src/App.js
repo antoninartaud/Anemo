f6b0d9eb29d2ddbb34d1e8d9ae478e01f4bb81e7
Mugi <g.mugi93140@gmail.com>
2021-07-05 12:20:11 +0200

    découpage du projet et création des dossiers

--- anemo-front/src/App.js
+++ anemo-front/src/App.js
@@ -1,23 +1,7 @@
-import './App.css';
-import React from 'react';
-import { BrowserRouter, Route, Link } from "react-router-dom";
-import 'styled-components'
-import NavBar from '../src/components/nav/NavBar';
-// import Form from './components/Modal/Form';
-// import HomePage from './views/HomePage';
 
 
-const App = () => {
-  return (
-    <div>
-      {/* <Form/> */}
-      {/* <HomePage/> */}
-      <NavBar/>
-    </div>
-  );
-}
 
-export default App;
+
 
 
 
