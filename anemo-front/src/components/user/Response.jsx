43af15b01f54c4006616076e6038b20abcb6ff7f
antonin <antoninartaud@hotmail.fr>
2021-07-13 23:08:50 +0200

    response version base

--- anemo-front/src/components/user/Response.jsx
+++ anemo-front/src/components/user/Response.jsx
@@ -12,113 +12,101 @@ export default function Response(props) {
   };
 
   return (
-    <div className='container'>
-      <div className='row'>
-        {/* <div className='form-check form-check-inline'> */}
-        <div className='btn-group'>
-          <input
-            // className='form-check-input '
-            className='btn-check '
-            type='radio'
-            name={props.questionsId}
-            id='inlineRadio1'
-            value='1'
-            onClick={handleChange}
-            // autocomplete='off'
-          />
-          {/* <label className='form-check-label' for='inlineRadio1'> */}
-          <label className='btn btn-outline-primary' for='inlineRadio1'>
-            1
-          </label>
-          {/* </div> */}
-          {/* <div className='form-check form-check-inline'> */}
-          {/* <div className='btn-group'> */}
-          <input
-            // className='form-check-input'
-            className='btn-check '
-            type='radio'
-            name={props.questionsId}
-            id='inlineRadio2'
-            value='2'
-            onChange={handleChange}
-            // autocomplete='off'
-          />
-          {/* <label className='form-check-label' for='inlineRadio2'> */}
-          <label className='btn btn-outline-primary' for='inlineRadio2'>
-            2
-          </label>
-          {/* </div> */}
-          {/* <div className='form-check form-check-inline'> */}
-          <input
-            className='btn-check '
-            type='radio'
-            name={props.questionsId}
-            id='inlineRadio3'
-            value='3'
-            onClick={handleChange}
-            // autocomplete='off'
-          />
-          <label className='btn btn-outline-primary' for='inlineRadio3'>
-            3
-          </label>
-          {/* </div> */}
-          {/* <div className='form-check form-check-inline'> */}
-          <input
-            className='btn-check '
-            type='radio'
-            name={props.questionsId}
-            id='inlineRadio4'
-            value='4'
-            onClick={handleChange}
-            // autocomplete='off'
-          />
-          <label className='btn btn-outline-primary' for='inlineRadio4'>
-            4
-          </label>
-          {/* </div> */}
-          {/* <div className='form-check form-check-inline'> */}
-          <input
-            className='btn-check '
-            type='radio'
-            name={props.questionsId}
-            id='inlineRadio5'
-            value='5'
-            onClick={handleChange}
-            // autocomplete='off'
-          />
-          <label className='btn btn-outline-primary' for='inlineRadio5'>
-            5
-          </label>
-          {/* </div> */}
-          {/* <div className='form-check form-check-inline'> */}
-          <input
-            className='btn-check '
-            type='radio'
-            name={props.questionsId}
-            id='inlineRadio6'
-            value='6'
-            onClick={handleChange}
-            // autocomplete='off'
-          />
-          <label className='btn btn-outline-primary' for='inlineRadio6'>
-            6
-          </label>
-          {/* </div> */}
-          {/* <div className='form-check form-check-inline'> */}
-          <input
-            className='btn-check'
-            type='radio'
-            name={props.questionsId}
-            id='inlineRadio7'
-            value='7'
-            onClick={handleChange}
-            // autocomplete='off'
-          />
-          <label className='btn btn-outline-primary' for='inlineRadio7'>
-            7
-          </label>
-          {/* </div> */}
-        </div>
+    <div>
+      <div className='form-check form-check-inline'>
+        <input
+          type='Agree'
+          className='form-check-input'
+          type='radio'
+          name={props.questionsId}
+          style={{ marginRight: '30px' }}
+          id='inlineRadio1'
+          value='1'
+          onClick={handleChange}
+        />
+        <label className='form-check-label' for='inlineRadio1'>
+          {' '}
+        </label>
+      </div>
+      <div className='form-check form-check-inline'>
+        <input
+          className='form-check-input'
+          type='radio'
+          name={props.questionsId}
+          style={{ marginRight: '30px' }}
+          id='inlineRadio2'
+          value='2'
+          onChange={handleChange}
+        />
+        <label className='form-check-label' for='inlineRadio2'></label>
+      </div>
+      <div className='form-check form-check-inline'>
+        <input
+          className='form-check-input'
+          type='radio'
+          name={props.questionsId}
+          style={{ marginRight: '30px' }}
+          id='inlineRadio3'
+          value='3'
+          onClick={handleChange}
+        />
+        <label className='form-check-label' for='inlineRadio3'>
+          {' '}
+        </label>
+      </div>
+      <div className='form-check form-check-inline'>
+        <input
+          className='form-check-input'
+          type='radio'
+          name={props.questionsId}
+          style={{ marginRight: '30px' }}
+          id='inlineRadio3'
+          value='4'
+          onClick={handleChange}
+        />
+        <label className='form-check-label' for='inlineRadio3'>
+          {' '}
+        </label>
+      </div>
+      <div className='form-check form-check-inline'>
+        <input
+          className='form-check-input'
+          type='radio'
+          name={props.questionsId}
+          style={{ marginRight: '30px' }}
+          id='inlineRadio3'
+          value='5'
+          onClick={handleChange}
+        />
+        <label className='form-check-label' for='inlineRadio3'>
+          {' '}
+        </label>
+      </div>
+      <div className='form-check form-check-inline'>
+        <input
+          className='form-check-input'
+          type='radio'
+          name={props.questionsId}
+          style={{ marginRight: '30px' }}
+          id='inlineRadio3'
+          value='6'
+          onClick={handleChange}
+        />
+        <label className='form-check-label' for='inlineRadio3'>
+          {' '}
+        </label>
+      </div>
+      <div className='form-check form-check-inline'>
+        <input
+          className='form-check-input'
+          type='radio'
+          name={props.questionsId}
+          style={{ marginRight: '30px' }}
+          id='inlineRadio3'
+          value='7'
+          onClick={handleChange}
+        />
+        <label className='form-check-label' for='inlineRadio3'></label>
       </div>
     </div>
   );
