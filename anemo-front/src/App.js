0d7ca8d71e35d9b4137b6964dbe9a40981122726
antonin <antoninartaud@hotmail.fr>
2021-07-13 22:58:35 +0200

    Revert "login signup formulaire design"
    This reverts commit ad434fd7d4838f52714d78cd2c7c66ad642605a5.

--- anemo-front/src/App.js
+++ anemo-front/src/App.js
@@ -1,91 +1,65 @@
-import React, { useState, useEffect } from 'react';
-import {
-  BrowserRouter,
-  Route,
-  Link,
-  Switch,
-  useHistory,
-} from 'react-router-dom';
+import React, { useState, useEffect } from 'react'
+import { BrowserRouter, Route, Link, Switch, useHistory } from "react-router-dom";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Admin from './views/Admin';
 import Add from './views/Add';
 import Edit from './views/Edit';
-import Signup from './views/Signup';
-import Login from './views/Login';
-import UserPage from './views/UserPage';
+import Signup from "./views/Signup";
+import Login from "./views/Login";
+import UserPage from "./views/UserPage";
 
 function App() {
-  const history = useHistory();
-  const [userConnected, setUserConnected] = useState(false);
-  const [informDisconnection, setInformDisconnection] = useState(false);
+    const history = useHistory()
+    const [userConnected, setUserConnected] = useState(false);
+    const [informDisconnection, setInformDisconnection] = useState(false);
 
-  useEffect(() => {
-    const token = localStorage.getItem('token') || false;
+    useEffect(() => {
+        const token = localStorage.getItem("token") || false
 
-    if (token) {
-      setUserConnected(true);
+        if (token) {
+            setUserConnected(true)
+        }
+    }, [])
+    const logout = () => {
+        localStorage.removeItem("token")
+        setUserConnected(false)
+        setInformDisconnection(true)
+
+        setTimeout(() => setInformDisconnection(false), 10000)
     }
-  }, []);
-  const logout = () => {
-    localStorage.removeItem('token');
-    setUserConnected(false);
-    setInformDisconnection(true);
 
-    setTimeout(() => setInformDisconnection(false), 10000);
-  };
+    return (
+        <BrowserRouter>
+            <div className='container'>
+                <div>
+                    <Link to="/">Signup</Link>
+                    <Link to="/login">Login</Link>
+                    <Link to="/user" ></Link>
+                    <Link to="/admin"></Link>
+                    <li className="nav-item">
+                        <a href="/" className="nav-link" onClick={logout}>Logout</a>
+                    </li>
 
-  return (
-    <BrowserRouter>
-      {/* Original Code Lines commmented - FG */}
-      {/* <div className='container'>
-        <div>
-          <Link to='/'>Signup</Link>
-          <Link to='/login'>Login</Link>
+                </div>
 
-          <li className='nav-item'>
-            <a href='/' className='nav-link' onClick={logout}>
-              Logout
-            </a>
-          </li> 
-        </div> */}
+                <Switch>
+                    <Route exact path='/admin' component={Admin} />
+                    <Route exact path='/edit/:id' component={Edit} />
+                    <Route exact path='/add' component={Add} />
+                    <Route exact path="/" >
+                        <Signup changeUserConnected={setUserConnected}></Signup>
+                    </Route>
+                    
+                    <Route exact path="/login" >
+                        <Login changeUserConnected={setUserConnected}></Login>
+                    </Route>
+                    <Route path="/user" exact component={UserPage}></Route>
 
-      <nav className='navbar navbar-expand-md bg-dark navbar-dark text-light'>
-        {/* class container to align the text inside the navbar to the grid */}
-        <div className='container'>
-          {/* class navbar-nav where all the links go */}
-          <div className='navbar-nav ms-auto '>
-            <Link className='nav-link ' to='/'>
-              Signup
-            </Link>
-            <Link className='nav-link' to='/login'>
-              Login
-            </Link>
-            <a className='nav-link' href='/' onClick={logout}>
-              Logout
-            </a>
-            <Link className='d-none' to='/user'></Link>
-            <Link className='d-none' to='/admin'></Link>
-          </div>
-        </div>
-      </nav>
-
-      <Switch>
-        <Route exact path='/admin' component={Admin} />
-        <Route exact path='/edit/:id' component={Edit} />
-        <Route exact path='/add' component={Add} />
-        <Route exact path='/'>
-          <Signup changeUserConnected={setUserConnected}></Signup>
-        </Route>
-        <Route exact path='/login'>
-          <Login changeUserConnected={setUserConnected}></Login>
-        </Route>
-        <Route path='/user' exact component={UserPage}></Route>
-      </Switch>
-      {/* </div> commented - FG */}
-      {/* </div> */}
-    </BrowserRouter>
-  );
+                </Switch>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
